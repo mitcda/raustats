5ad5505014072ff3268c19c89db9aed5e6875275
= <=>
2018-09-28 22:34:25 +1000

    Added package data
    Added rba_tablecache, abs_tablecache, aus_state_codes

---
+++ data-raw/build-data.R
@@ -0,0 +1,26 @@
+### =========================================================================
+### Filename:     build-data.R
+### Created:      2017-08-10
+### Updated:      <2018-09-28 22:27:36 david at grover>
+### Author:       David Mitchell <david.p.mitchell@homemail.com.au>
+### Description:  Builds package data sets
+### =========================================================================
+
+######  Section 0 - Libraries & settings
+## library(magrittr);
+## library(dplyr);
+## library(tidyr);
+
+### Settings
+DEBUG <- FALSE
+
+######  Section 1 - Import exports and imports table structure
+devtools::load_all("./");
+rba_tablecache <- rba_table_cache();
+abs_tablecache <- read.csv(file.path("../data-raw", "ABS-TSS-Catalogue-Numbers.csv"));
+aus_state_codes <- read.csv(file.path("../data-raw", "Australian-States-Territories.csv"));
+
+## Write data sets files
+devtools::use_data(rba_tablecache, abs_tablecache, aus_state_codes, overwrite=TRUE);
+
+### =============================== EOF =====================================
