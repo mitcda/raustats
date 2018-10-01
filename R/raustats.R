a567ea13d93a46bb9def091b0223bc95302e643b
= <=>
2018-10-01 21:54:06 +1000

    Added vignette directory
    Added raustats.R package and vignette directory.

---
+++ R/raustats.R
@@ -0,0 +1,12 @@
+#' raustats: An R package for accessing data and statistics from the ABS and RBA websites
+#'
+#' The raustats package provides structured access to all data and statistics 
+#' available from the Australian Bureau of Statistics and Reserve Bank of Australia
+#' website, as well as draft access to the ABS.Stat - Beta data catalogue API.
+#'
+#' To learn more about the raustats package, start with the vignettes:
+#' \code{browseVignettes(package = "raustats")}
+#'
+#' @docType package
+#' @name raustats
+NULL
