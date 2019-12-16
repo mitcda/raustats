b1b15d0663e3193edc5779f9102b6b655c7e20ef
= <=>
2019-12-16 16:29:05 +1100

    Added separate function to check ABS/RBA URL validity

---
+++ R/httr-utilities.R
@@ -0,0 +1,36 @@
+## httr settings
+#' @name raustats_ua
+#' @title raustats package user agent
+#' @description This function specifies the package user agent, and is used inside
+#'   GET/POST function calls
+#' @importFrom httr user_agent
+#' @return a list with a base url and a url section for formatting the JSON API calls
+#' @author David Mitchell <david.pk.mitchell@@gmail.com>
+#' @keywords internal
+raustats_ua <- function()
+  user_agent("http://github.com/mitcda/raustats")
+
+
+
+#' Check if the specified ABS/RBA URL is available
+#' @name raustats_check_url_available
+#' @title Check specified ABS/RBA URL available
+#' @description Function to ensure URL calls fail gracefully with an informative message if the
+#'   resource is not available (and not give a check warning nor error).
+#' @importFrom httr GET status_code
+#' @param url The base URL to check.
+#' @keywords internal
+#' @return \code{TRUE} if the API is available, otherwise \code{stop()} is called.
+#' @note Based on code in \code{opensensmapR}  \url{ttps://github.com/sensebox/opensensmapR/blob/f69cf62b2771d5b6ed605c04b7ddd618f5a272c2/R/api.R}{\code{api.R}}.
+raustats_check_url_available <- function(url) {
+  code = FALSE
+  try({ code = status_code(GET(url, raustats_ua())) }, silent = TRUE)
+  
+  if (code == 200)
+    return(TRUE)
+  
+  if (code != FALSE)
+    errtext = sprintf("The API at %s is currently not available. (HTTP code %s)", url, code)
+  stop(paste(errtext, collapse='\n'), call. = FALSE)
+  FALSE
+}
---
+++ tests/testthat/test-url-availability.R
@@ -0,0 +1,51 @@
+context("raustats URL checking functions")
+
+test_that("raustats_check_api_available returns true results",
+{
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
+  ## Check valid ABS Catalogue URLs
+  expect_true(raustats_check_api_available(abs_urls()$base_url))
+  expect_true(raustats_check_api_available(file.path(abs_urls()$base_url,
+                                                     abs_urls()$ausstats_path,
+                                                     mf_path = "mf")));
+  expect_true(raustats_check_api_available(file.path(abs_urls()$base_url,
+                                                     abs_urls()$ausstats_path,
+                                                     abs_urls()$mf_path,
+                                                     "5206.0")));
+
+  ## Check valid ABS API URLs
+  expect_true(raustats_check_api_available(file.path(abs_api_urls()$base_url)));
+
+  ## Check valid RBA paths
+  expect_true(raustats_check_api_available(rba_urls()$base_url));
+  expect_true(raustats_check_api_available(file.path(rba_urls()$base_url,
+                                                     rba_urls()$stats_path)));
+  expect_true(raustats_check_api_available(file.path(rba_urls()$base_url,
+                                                     rba_urls()$stats_path,
+                                                     rba_urls()$tables_path)));
+})
+
+
+test_that("raustats_check_api_available fails gracefully",
+{
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
+  ## Test mis-specified ABS Catalogue URL
+  expect_error(raustats_check_api_available(file.path(abs_urls()$base_url,
+                                                      abs_urls()$ausstats_path)));
+  
+  ## Test mis-specified ABS API URL
+  expect_error(raustats_check_api_available(file.path(abs_api_urls()$base_url,
+                                                      abs_api_urls()$datastr_path,
+                                                      abs_api_urls()$sdmx_json_path)));
+  
+  ## Test mis-specified RBA URLs
+  expect_error(raustats_check_api_available(file.path(rba_urls()$base_url,
+                                                      rba_urls()$stats_path,
+                                                      "Table_1")));
+})
